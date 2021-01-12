f7680ab609d0ad42d352f11e0302cd505821beed
rinharis <rinharis@gmail.com>
2021-01-12 12:59:45 +0100

    WIP : De devient statique

--- Tp_ProjetArmello/De.cs
+++ Tp_ProjetArmello/De.cs
@@ -4,17 +4,12 @@ using System.Text;
 
 namespace Tp_ProjetArmello
 {
-    static class De
+    public static class De
     {
-        //FIXIT : comment encapsuler un attribut static dans une classe statique
-        private static int valeur;
-        public static int GetValeur()
-        {
-            return De.valeur;
-        }
         public static int Lance()
         {
-            return new Random().Next(1, 7);
+            Random random = new Random();
+            return random.Next(1, 7);
         }
     }
 }
--- Tp_ProjetArmello/Program.cs
+++ Tp_ProjetArmello/Program.cs
@@ -9,14 +9,14 @@ namespace Tp_ProjetArmello
             int test;
             
             //FIXIT : La valeur de De n'est pas modifiee
-            Console.WriteLine("De.GetValeur : " + De.GetValeur());
+            Console.WriteLine("De.GetValeur : " + De.Lance());
             De.Lance();
-            Console.WriteLine("De.GetValeur : " + De.GetValeur());
+            Console.WriteLine("De.GetValeur : " + De.Lance());
             De.Lance();
-            Console.WriteLine("De.GetValeur : " + De.GetValeur());
-            Gobelet g1 = new Gobelet();
-            g1.LanceDe(8);
-            Console.WriteLine("Goblelet :" + g1.GetDes);
+            Console.WriteLine("De.GetValeur : " + De.Lance());
+            //Gobelet g1 = new Gobelet();
+            //g1.LanceDe(8);
+            //Console.WriteLine("Goblelet :" + g1.GetDes());
 
 
         }
